4c2b9e87ea791977fa799a1e1680708e09e8ca95
Your Name <you@example.com>
2022-06-07 22:11:36 +0530

    spring boot application

---
+++ student-application (2)/student-application/src/main/java/com/student/entity/Student.java
@@ -0,0 +1,76 @@
+package com.student.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student_pavneet")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer studentId;
+	@Column(name = "student_name")
+	private String name;
+	@Column(name = "mobile_number", length = 10)
+	private String mobileNumber;
+	@Column(name = "email_id")
+	private String emailId;
+	@Column(name = "login_id", unique = true)
+	private String longId;
+	@Column(name = "password")
+	private String password;
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getLongId() {
+		return longId;
+	}
+
+	public void setLongId(String longId) {
+		this.longId = longId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
